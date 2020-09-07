85a8fd5abc7bca4ddfc28548fb7117cd05f8816a
smhrd <smhrd@DESKTOP-DSDFOR4>
2020-09-07 16:39:49 +0900

    - 길동이에게 작업요청

--- webmvc/src/kr/smhrd/MyUtil.java
+++ webmvc/src/kr/smhrd/MyUtil.java
@@ -3,10 +3,8 @@ package kr.smhrd;
 public class MyUtil {
 	// 1~10까지의 총합을 구하는 동작
 	public int hap() {
-		int sum = 0;
-		for (int i = 1; i <=10; i++) {
-			sum+=i;
-		}
-		return sum;
+		// 길동아 여기  좀 수정해봐
+		
+		return 55; 
 	}
 }

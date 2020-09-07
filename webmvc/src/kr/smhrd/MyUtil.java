fd4822c258442a1a9eb7a64a5a88d6c392ec3c31
smhrd <smhrd@DESKTOP-DSDFOR4>
2020-09-07 16:44:39 +0900

    -수정완료

--- webmvc/src/kr/smhrd/MyUtil.java
+++ webmvc/src/kr/smhrd/MyUtil.java
@@ -3,8 +3,11 @@ package kr.smhrd;
 public class MyUtil {
 	// 1~10까지의 총합을 구하는 동작
 	public int hap() {
-		// 길동아 여기  좀 수정해봐
+		int sum = 0;
+		for (int i = 1; i <= 100; i++) {
+			sum +=i;
+		}
 		
-		return 55; 
+		return sum; 
 	}
 }
